da8b5907ea247468696bd24a25cc2bd76e83dbfb
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-17 16:57:47 +0200

    első PlantUML

---
+++ src/main/java/csomag/Tipusok.java
@@ -0,0 +1,17 @@
+package csomag;
+
+import java.util.Scanner;
+
+public class Tipusok {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+        double b;
+        String c = "";
+        c += b = a = 100/7;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        s.nextLine();
+    }
+}
